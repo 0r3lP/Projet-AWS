21dbcf0ca4e41ea44314d59aecb3bb19286c1431
0r3lP <a.poelger@ecole-ipssi.net>
2024-07-23 16:14:57 +0200

    refresh

--- App/main.tf
+++ App/main.tf
@@ -1,1 +1,30 @@
-aa
+# Module pour la configuration réseau 
+module "network" {
+  source = "../Modules/network"
+}
+
+# Module pour le groupe de sécurité
+module "security_group" {
+  source = "../Modules/security-group"
+}
+
+# Module pour le cluster ECS
+module "ecs_cluster" {
+  source = "../Modules/ecs-cluster"
+  subnet_ids = module.network.subnet_ids
+}
+
+# Module pour le volume EBS
+module "volume_ebs" {
+  source = "../Modules/volume-ebs"
+}
+
+# Module pour le service ECS
+module "ecs_service" {
+  source = "../Modules/ecs-service"
+  cluster_id    = module.ecs_cluster.cluster_id
+  security_group_id = module.security_group.id
+  subnet_ids    = module.network.subnet_ids
+  ebs_volume_id = module.volume_ebs.id
+}
+
